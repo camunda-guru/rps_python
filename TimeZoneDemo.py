5aa31c6891fa7dd799f52656d251f1e498e2ba62
eswaribala <Parameswaribala@gmail.com>
2017-02-15 17:14:11 +0530

    Add files via upload

---
+++ TimeZoneDemo.py
@@ -0,0 +1,27 @@
+'''
+Created on 13-Feb-2017
+
+@author: BALASUBRAMANIAM
+'''
+from datetime import datetime
+from pytz import timezone
+from pytz import all_timezones
+
+date_str = datetime.today()
+datetime_obj_utc = date_str.replace(tzinfo=timezone('UTC'))
+print (datetime_obj_utc.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+datetime_obj_utc = date_str.replace(tzinfo=timezone('US/Pacific'))
+print (datetime_obj_utc.strftime("%Y-%m-%d %H:%M:%S %Z%z"))
+
+for zone in all_timezones:
+    if 'US' in zone:
+        print (zone)
+        
+from datetime import datetime
+from pytz import timezone
+
+fmt = "%Y-%m-%d %H:%M:%S %Z%z"
+timezonelist = ['UTC','US/Pacific','Europe/Berlin']
+for zone in timezonelist:
+    now_time = datetime.now(timezone(zone))
+    print (now_time.strftime(fmt))
